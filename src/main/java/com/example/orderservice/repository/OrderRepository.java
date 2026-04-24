2892442e440ef1d5e8016fe0a9c65b2cb8ac66d4
sowmea <sowmahe993@gmail.com>
2026-04-23 20:30:30 -0400

    Fixed bugs

--- src/main/java/com/example/orderservice/repository/OrderRepository.java
+++ src/main/java/com/example/orderservice/repository/OrderRepository.java
@@ -1,6 +1,14 @@
 package com.example.orderservice.repository;
 
 
+import com.example.orderservice.model.Order;
+import feign.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.awt.print.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
